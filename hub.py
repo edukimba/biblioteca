3ee10d15e4e546a60b3f2518b4eba851084c9550
Eduardo Kimba <eduardo3287.santos@gmail.com>
2025-10-06 00:57:04 -0300

    adicionando novas funções

--- hub.py
+++ hub.py
@@ -1,7 +1,7 @@
 #LISTAS:
 
 USUARIOS = []
-LIVROS =[]
+LIVROS = []
 EMPRESTADOS = [] 
 
 #HUB DE OPÇÕES:
@@ -9,23 +9,24 @@ EMPRESTADOS = []
 def hub():
     while True:
         print("\n---BIBLIOTECA---")
-        print("1. ADICIONAR NOVOS LIVRO")
-        print("2. REMOVER LIVRO")
-        print("3. LISTAR LIVROS DISPONÍVEIS")
-        print("4. REGISTRAR USUÁRIOS")
-        print("5. REGISTRAR EMPRÉSTIMOS DE LIVROS")
+        print("1. ADICIONAR UM NOVO LIVRO")
+        print("2. REMOVER UM LIVRO")
+        print("3. LISTAR OS LIVROS DISPONÍVEIS")
+        print("4. REGISTRAR UM NOVO USUÁRIO")
+        print("5. REGISTRAR EMPRÉSTIMO DE LIVRO")
         print("6. CONSULTAR LIVROS DISPONÍVEIS E EMPRESTADOS")
         print("0. SAIR")
+        print("---------------")
 
         opcao = input("ESCOLHA UMA OPÇÃO:")
         
-#OPÇÃO 1
+#OPÇÃO 1:
 
         if opcao == "1":
             print("---------------")
-            print(f"VOCÊ ESCOLHEU ADICIONAR NOVO LIVRO!")
+            print(f"VOCÊ ESCOLHEU ADICIONAR UM NOVO LIVRO!")
             print("---------------")
-            livro_novo = input("DIGITE O NOME DO LIVRO NOVO:")
+            livro_novo = input("DIGITE O NOME DO LIVRO NOVO:").strip().lower()
             print("---------------")
             if livro_novo in LIVROS:
                 print("LIVRO JÁ CADASTRADO!")
@@ -39,4 +40,93 @@ def hub():
             print("---------------")
             print("VOCÊ ESCOLHEU REMOVER UM LIVRO!")
             print("---------------")
-            nome = input("DIGITE O NOME DO LIVRO A SER REMOVIDO")
+            livro = input("DIGITE O NOME DO LIVRO:" ).strip().lower()
+            if livro in LIVROS:
+                print("---------------")
+                LIVROS.remove(livro)
+                print(f"LIVRO'{livro.title()}' REMOVIDO")
+            else:
+                print("---------------")
+                print("LIVRO NÃO ENCONTRADO!")
+
+#OPÇÃO 3:
+
+        elif opcao == "3":
+            print("---------------")
+            print("VOCÊ ESCOLHEU LISTAR LIVROS DISPONÍVEIS!")
+            print("---------------")
+            if len(LIVROS) == 0:
+                print("NÃO HÁ LIVROS!")
+            else:
+                for i in range(len(LIVROS)):
+                    print(f"{i} - {LIVROS[i].title()}")
+
+#OPÇÃO 4:
+
+        elif opcao == "4":
+            print("---------------")
+            print(f"VOCÊ ESCOLHEU ADICIONAR UM NOVO USUÁRIO!")
+            print("---------------")
+            usuario_novo = input("DIGITE O NOME DO USUÁRIO NOVO:").strip().lower()
+            print("---------------")
+            if usuario_novo in USUARIOS:
+                print("USUÁRIO JÁ CADASTRADO!")
+            else:
+                print("USUÁRIO ADICIONADO!")
+                USUARIOS.append(usuario_novo)
+
+#OPÇÃO 5:
+
+        elif opcao == "5":
+            print("---------------")
+            print("VOCÊ ESCOLHEU REGISTRAR UM EMPRÉSTIMO!")
+            print("---------------")
+            livro_emprestado = input("DIGITE O NOME DO LIVRO A SER EMPRESTADO:").strip().lower()
+            if livro_emprestado in LIVROS:
+                usuario = input("DIGITE O NOME DO USUÁRIO:").strip().lower()
+                if usuario in USUARIOS:
+                    LIVROS.remove(livro_emprestado)
+                    EMPRESTADOS.append({"livro": livro_emprestado, "usuario": usuario})
+                    print("---------------")
+                    print(f"EMPRÉSTIMO REGISTRADO PARA {usuario.title()}!")
+                else:
+                    print("---------------")
+                    print("USUÁRIO NÃO ENCONTRADO!")
+            elif any(e["livro"] == livro_emprestado for e in EMPRESTADOS):
+                print("---------------")
+                print("LIVRO JÁ ESTÁ EMPRESTADO!")
+            else:
+                print("---------------")
+                print("LIVRO NÃO ENCONTRADO!")
+
+#OPÇÃO 6:
+
+        elif opcao == "6":
+            print("---------------")
+            print("VOCÊ ESCOLHEU VER LIVROS EMPRESTADOS E DISPONÍVEIS!")
+            print("---------------")
+            if len(LIVROS) == 0 and len(EMPRESTADOS) == 0:
+                print("NÃO HÁ LIVROS!")
+            else:
+                print("LIVROS DISPONÍVEIS:")
+                for i, livro in enumerate(LIVROS):
+                    print(f"{i} - {livro.title()}")
+                print("---------------")
+                print("LIVROS EMPRESTADOS:")
+                for i, livro in enumerate(EMPRESTADOS):
+                   print(f"{i} - {livro['livro'].title()} (EMPRESTADO PARA {livro['usuario'].title()})")
+
+#OPÇÃO 0:
+
+        elif opcao == "0":
+            print("---------------")
+            print("SAINDO...")
+            break
+
+#OPÇÃO INEXISTENTE SERÁ TRATADA ASSIM:
+
+        else:
+            print("---------------")
+            print("OPCÃO INVÁLIDA!")
+
+hub()
